412856bd0f1bb15e75077fe5a2194b2b98ca910f
gizmo <73662348+gizmo87898@users.noreply.github.com>
2024-01-07 17:58:23 -0500

    Update Ford Focus mk3 cluster controller v2.py

--- Ford Focus mk3 cluster controller v2.py
+++ Ford Focus mk3 cluster controller v2.py
@@ -15,7 +15,7 @@ start_time_100ms = time.time()
 start_time_10ms = time.time()
 start_time_5s = time.time()
 
-id_counter = 0x81
+id_counter = 0
 
 rpm = 2000
 speed = 6108
@@ -42,12 +42,12 @@ compass = 0x30
 # W = ?
 # NW = ?
 
-hdc = False
-tpms = False
-cruise_control = False
-cruise_control_speed = 80
-foglight = True
-parking_lights = False
+hdc = False # not recved from game
+tpms = False #tbd
+cruise_control = False # tbd
+cruise_control_speed = 80 # tbd
+foglight = True #works, not recved from game
+parking_lights = False #doesnt work, also not revced from game
 rear_foglight = False # works
 check_engine = False
 driver_door = False
@@ -102,17 +102,17 @@ while True:
             
     # Send each message every 100ms
     elapsed_time_100ms = current_time - start_time_100ms
-    if elapsed_time_100ms >= 0.1:
+    if elapsed_time_100ms >= 0.05:
         messages_100ms = [
             
             can.Message(arbitration_id=0x3a, data=[ # Directionals
                 0x03,(right_directional*8) + (left_directional*4),0x01,0xff,0x81,0xff,0xff,0xff], is_extended_id=False),
             
             can.Message(arbitration_id=0x40, data=[ # Airbag light, odometer increment
-                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=False),
+                0xFF, random.randint(0,255), 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=False),
             
             can.Message(arbitration_id=0x60, data=[ # Seatbelt
-                0xd0,0,0,0,0,0,0,0], is_extended_id=False),
+                seatbelt*96,0,0,0,0,0,0,0], is_extended_id=False),
             
             can.Message(arbitration_id=0x70, data=[ # ABS/TC
                 0x00, 0x98, 0x4, 0x80, 0x00, 0xF4, 0xE8, 0x10], is_extended_id=False),
@@ -135,32 +135,28 @@ while True:
             can.Message(arbitration_id=0x230, data=[ # TPMS
                 0x0,0x95,0x04,0x82,0,0,0,0], is_extended_id=False),
             
-            can.Message(arbitration_id=0x240, data=[ # water in fuel message
+            can.Message(arbitration_id=0x240, data=[ # handbrake
                 0xF5, 0x00, 0x00, 0x40, 0x00, 0x9F, 0x80, 0xD1], is_extended_id=False),
             
             can.Message(arbitration_id=0x250, data=[ # Oil Pressure, MIL, Backlight Brightness
                 0xFF, 0x10, 0x15, 0x03, 0x14, 0x10, 0x0E, 0x31], is_extended_id=False),
             
-            can.Message(arbitration_id=0x290, data=[ # Brake fluid error message (from ECU?)
-                0x93, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x13], is_extended_id=False),
             
-            can.Message(arbitration_id=0x2a0, data=[ # Engine service (?)
-                70, 0xFF, 0x80, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=False),
             
             can.Message(arbitration_id=0x300, data=[ # Trans. Malfunction, Battery light
                 0,0,0,0,0xff,0,0,0], is_extended_id=False),
             
             can.Message(arbitration_id=0x320, data=[ #Fuel 
-                0xe, 0xe, 30, 0, 0xe, 0xe, 0xe, 0xe], is_extended_id=False),
+                0xe, 0xe, 0x1c, 2, 0xe, 0xe, 0xe, 0xe], is_extended_id=False),
             
             can.Message(arbitration_id=0x360, data=[ # Engine Temp
-                0x00, 0x00, 0x00, 0x22, 0x46, 0xFE, 0x22, 0x22], is_extended_id=False),
+                0x00, 0x00, 0x00, 0x44, 0x80, 0xFE, 0x22, 0x22], is_extended_id=False),
             
             can.Message(arbitration_id=0x466, data=[ # Compass
                 0xff,0xff,0x00,0x22,0x50,0xff,0,0], is_extended_id=False),
             
-            can.Message(arbitration_id=id_counter, data=[
-                random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255)], is_extended_id=False),
+            #can.Message(arbitration_id=id_counter, data=[
+            #    random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255)], is_extended_id=False),
         ]
         
         for message in messages_100ms:
@@ -177,17 +173,20 @@ while True:
             can.Message(arbitration_id=0x110, data=[ 
                 0xff,0xff,0,0,(int(rpm/2))>>8,(int(rpm/2))&0xff,(speed&0xff00)>>8,speed&0xff], is_extended_id=False),
             
-            can.Message(arbitration_id=0x290, data=[
-                0xFF,0,0,0,0b10101010,0,0,0], is_extended_id=False),
+
+            #can.Message(arbitration_id=0x20, data=[ # Gear
+            #    0x00, 0x00, 0x1, 0x00, 0x1, 0x00, 0x00, 0x00], is_extended_id=False),
             
-            can.Message(arbitration_id=0x20, data=[ # Gear
-                0x00, 0x00, 0x1, 0x00, 0x1, 0x00, 0x00, 0x00], is_extended_id=False),
+            can.Message(arbitration_id=0x290, data=[ # Brake fluid error message (from ECU?)
+                0x93, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x13], is_extended_id=False),
             
+            can.Message(arbitration_id=0x2a0, data=[ # Engine service (?)
+                70, 0xFF, 0x80, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], is_extended_id=False),
         ]
         
         for message in messages_10ms:
             bus.send(message)
-            wpt.sleep(0.001)
+            wpt.sleep(0.002)
         start_time_10ms = time.time()
 
     # Execute code every 5s
@@ -195,6 +194,32 @@ while True:
     if elapsed_time_5s >= 3:
         id_counter += 1
         print(hex(id_counter))
+        
+        rpm = random.randint(0,8000)
+        speed = random.randint(0,24500)
+        hdc = not hdc
+        tpms = not tpms
+        cruise_control = not cruise_control
+        foglight = not foglight
+        parking_lights = not parking_lights
+        rear_foglight = not rear_foglight
+        check_engine = not check_engine
+        driver_door = not driver_door
+        passenger_door = not passenger_door
+        driver_rear_door = not driver_rear_door
+        passenger_rear_door = not passenger_rear_door
+        hood = not hood
+        trunk = not trunk
+        airbag = not airbag
+        seatbelt = not seatbelt
+        left_directional = not left_directional
+        right_directional = not right_directional
+        tc = not tc
+        abs = not abs
+        battery = not battery
+        handbrake = not handbrake
+        highbeam = not highbeam
+
         start_time_5s = time.time()
 
 sock.close()
